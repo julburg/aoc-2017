1fe8a3a87b014593224a2c6599cec85f4afc3a17
Julia Burgard <burgard@synyx.de>
2017-12-22 13:38:47 +0100

    Add solution for 12b

--- src/main/kotl/de/aoc/Day12.kt
+++ src/main/kotl/de/aoc/Day12.kt
@@ -2,19 +2,41 @@ package de.aoc
 
 import kotl.de.aoc.findAll
 
-/**
- * @author  Julia Burgard - burgard@synyx.de
- */
 fun day12(inputAsString: String): Int {
+    val input = getInput(inputAsString)
+    return getAllInGroup(0, input).size
+
+}
+
+fun day12b(inputAsString: String): Int {
+    val input = getInput(inputAsString)
+
+    val numbers = input.map { it.number }.toMutableList();
+    var groupCount = 0;
+    while (!numbers.isEmpty()) {
+        val visited = getAllInGroup(numbers[0], input)
+        numbers.removeAll(visited)
+        groupCount++;
+    }
+
+
+    return groupCount
+
+}
+
+private fun getInput(inputAsString: String): Sequence<Input> {
     val input = inputAsString.findAll("""(\d*) <-> (.*)""").map {
         val (number: String, connections: String) = it.destructured
         Input(number.toInt(), connections.split(",").map { it.trim() }.map { it.toInt() })
     }
+    return input
+}
 
+private fun getAllInGroup(start: Int, input: Sequence<Input>): HashSet<Int> {
     val visited = HashSet<Int>();
     val toVisit = HashSet<Int>();
 
-    toVisit.add(0)
+    toVisit.add(start)
 
     while (!toVisit.isEmpty()) {
         val nextElement = toVisit.first()
@@ -26,9 +48,7 @@ fun day12(inputAsString: String): Int {
 
         toVisit.addAll(elmentsLeftToProcess)
     }
-
-    return visited.size
-
+    return visited
 }
 
 data class Input(var number: Int, var values: List<Int>) {
--- src/test/kotl/de/aoc/Day12KtTest.kt
+++ src/test/kotl/de/aoc/Day12KtTest.kt
@@ -28,7 +28,30 @@ class Day12KtTest {
 
         val day12 = day12(input)
         println(day12)
-        assert(day12 == 0)
+        assert(day12 == 378)
+    }
+
+    @Test
+    fun day12bExample() {
+        val input = "0 <-> 2\n" +
+                "1 <-> 1\n" +
+                "2 <-> 0, 3, 4\n" +
+                "3 <-> 2, 4\n" +
+                "4 <-> 2, 3, 6\n" +
+                "5 <-> 6\n" +
+                "6 <-> 4, 5\n"
+
+        val day12 = day12b(input)
+        assert(day12 == 2)
+    }
+
+    @Test
+    fun day12b() {
+        val input = File("inputDay12").inputStream().bufferedReader().use { it.readText() }
+
+        val day12 = day12b(input)
+        println(day12)
+        assert(day12 == 204)
     }
 
 
