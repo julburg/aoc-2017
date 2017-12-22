e7cc1394399a659f11daaae87cafe6cc7b82b8a3
Julia Burgard <burgard@synyx.de>
2017-12-22 11:49:53 +0100

    Add solution for 10a

--- src/main/kotl/de/aoc/Day10.kt
+++ src/main/kotl/de/aoc/Day10.kt
@@ -1,19 +1,57 @@
 package de.aoc
 
+import kotlin.math.min
+
 /**
  * @author  Julia Burgard - burgard@synyx.de
  */
 fun day10(input: String, elementCount: Int): Int {
 
 
-    val data = Array(elementCount - 1, {i -> i})
-    println(data)
-    var pointer=0;
+    val data = Array(elementCount, { i -> i }).toMutableList()
+    var pointer = 0
+    var skipSize = 0
 
-//    input.split(",").forEach(
-//
-//    );
+    input.split(",").forEach {
+        val subList = getSubList(data, pointer, it.toInt())
+        val reversed = subList.values.reversed();
 
-    return 0
+        subList.indizes.forEachIndexed { i, indize ->
+            data[indize] = reversed[i]
+        }
+
+        pointer = (pointer + it.toInt() + skipSize) % data.size
+        skipSize++;
+
+        println("res: " + data.joinToString(","))
+
+
+    };
+
+    return data[0] * data[1]
 
 }
+
+fun getSubList(list: List<Int>, pointer: Int, lengthToAllocate: Int): SubList {
+    val start = pointer
+    val end = min(list.size, pointer + lengthToAllocate)
+
+    val firstSubList = list.subList(start, end)
+    val firstIndizes = (start..end - 1).toList()
+
+    if (firstSubList.size < lengthToAllocate) {
+        val secondSubListLength = lengthToAllocate - firstSubList.size
+        val secondSubList = list.subList(0, secondSubListLength)
+        val secondIndizes = (0..secondSubListLength - 1).toList()
+        return SubList((firstIndizes.toList() + secondIndizes.toList()).toList(), (firstSubList.toList() + secondSubList.toList()).toList())
+
+    }
+
+    return SubList(firstIndizes, firstSubList)
+
+
+}
+
+data class SubList(var indizes: List<Int>, var values: List<Int>)
+
+
--- src/test/kotl/de/aoc/Day10KtTest.kt
+++ src/test/kotl/de/aoc/Day10KtTest.kt
@@ -2,6 +2,7 @@ package de.aoc
 
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.*
 
 /**
  * @author Julia Burgard - burgard@synyx.de
@@ -19,11 +20,42 @@ class Day10KtTest {
 
     @Test
     fun day10Example() {
-        val input = "3, 4, 1, 5"
+        val input = "3,4,1,5"
 
         val day10 = day10(input, 5)
         println(day10)
-        assertTrue(day10 == 1)
+        assertTrue(day10 == 12)
+    }
+
+    @Test
+    fun getSubListCircular() {
+        val input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+
+        val res = getSubList(input, 5, 5)
+        println(res.indizes.joinToString(","))
+        println(res.values.joinToString(","))
+
+        assertTrue(res.indizes == Arrays.asList(5, 6, 7, 0, 1))
+        assertTrue(res.values == Arrays.asList(6, 7, 8, 1, 2))
+    }
+
+    @Test
+    fun getSubList() {
+        val input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+
+        val res = getSubList(input, 2, 3)
+        assertTrue(res.indizes == Arrays.asList(2, 3, 4))
+        assertTrue(res.values == Arrays.asList(3, 4, 5))
+    }
+
+    @Test
+    fun getSubListOneElement() {
+        val input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+
+        val res = getSubList(input, 7, 1)
+        println(res.values.joinToString(","))
+        assertTrue(res.indizes == Arrays.asList(7))
+        assertTrue(res.values == Arrays.asList(8))
     }
 
 
